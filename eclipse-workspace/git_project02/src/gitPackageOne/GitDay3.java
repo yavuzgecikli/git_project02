237bff76b398593caa6c88885e57af67be091971
Yavuz <yavuzgecikli@hotmail.com>
2021-05-21 13:29:33 -0400

    first commit on local feature branch

--- eclipse-workspace/git_project02/src/gitPackageOne/GitDay3.java
+++ eclipse-workspace/git_project02/src/gitPackageOne/GitDay3.java
@@ -4,6 +4,7 @@ public class GitDay3 {
 	public static void main(String[] args) {
 		
 	  System.out.println("1st changes on local master");
+	  System.out.println("This is my feature branch");
 		
 	}
 
