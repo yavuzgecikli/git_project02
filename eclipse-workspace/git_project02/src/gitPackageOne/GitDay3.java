eda5bb3093f6c65b683a76313c3d4fff38f1b94a
Yavuz <yavuzgecikli@hotmail.com>
2021-05-23 08:06:39 -0400

    Third commit from local branch

--- eclipse-workspace/git_project02/src/gitPackageOne/GitDay3.java
+++ eclipse-workspace/git_project02/src/gitPackageOne/GitDay3.java
@@ -6,6 +6,7 @@ public class GitDay3 {
 	  System.out.println("1st changes on local master");
 	  System.out.println("This is my feature branch");
 	  System.out.println("My second push from local feature branch");
+	  System.out.println("My third push from local feature branch");
 		
 	}
 
